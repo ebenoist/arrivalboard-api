1a8eb6c34a6e356e037ea95e671f948fe85cd897
Erik Benoist <ebenoist@gmail.com>
2016-08-24 20:45:21 -0500

    Add new trips

--- config/deploy.sh
+++ config/deploy.sh
@@ -5,5 +5,7 @@ RUN_CMD="docker run -d \
   -e ENV=${ENV} \
   -e TT_KEY=${TT_KEY} \
   -e BUS_KEY=${BUS_KEY} \
+  -e RTD_USER=${RTD_USER} \
+  -e RTD_PASS=${RTD_PASS} \
   --link ab-mongo:mongo -p 8080:3000 \
   --name ab ebenoist/arrivalboard-api:release"
